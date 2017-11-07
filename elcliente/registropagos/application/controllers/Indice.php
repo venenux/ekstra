6be935f7b56e0304448e708a2f272e83110f054e
PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2017-11-07 02:49:26 -0400

    ampliado login cliente, modulo definido parametro y index en routers

---
+++ elcliente/registropagos/application/controllers/Indice.php
@@ -0,0 +1,39 @@
+<?php //controlador index para elcliente
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Indice extends CI_Controller
+{
+
+	public function __construct()
+	{
+			parent::__construct();
+			$this->load->library('session');
+			$this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT',TRUE);
+			$this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0', TRUE);
+			$this->output->set_header('Pragma: no-cache', TRUE);
+			$this->output->set_header("Expires: Mon, 26 Jul 1997 05:00:00 GMT", TRUE);
+			$this->output->enable_profiler(TRUE);
+	}
+
+
+	public function index()
+	{
+			if( $this->session->userdata('logueado') == FALSE)
+			{ 
+				$this->load->helper('url');
+				redirect('login_usuario/iniciarsesion/?errl=inicie login');
+			}
+			else
+			{  
+				$sDatausuario['controlador']	='RegistroPagos ';
+				$this->load->helper(array('form', 'url','html'));
+				$this->load->view('view_header');
+				$this->session->set_userdata($sDatausuario);
+				$this->load->helper('url');
+				redirect('Registropagos/index', 'refresh');
+			} 
+	}
+
+}
+?>
--- elcliente/registropagos/application/views/iniciosesion.php
+++ elcliente/registropagos/application/views/iniciosesion.php
@@ -1,15 +1,19 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-		if ( !isset($paso) ) $paso = '';
+		if ( !isset($errl) ) $errl = '';
 		$htmlformaattributos = array('name'=>'formulariomanejousuarios','class'=>'formularios','onSubmit'=>'return validageneric(this);');
 
 		echo '<h2><center>Registro de pagos: Inicie Sesión</center></h2>';
 		echo form_open('login_usuario/iniciarsesion', $htmlformaattributos) . PHP_EOL;
 		echo 'Usuario:'.form_input('username').PHP_EOL;
-		echo form_hidden('paso',$paso).PHP_EOL;
 		echo 'Clave :'.form_password('contrasena').PHP_EOL; 
-		$moduloindexarray = array('1'=>'1','2'=>'2');
-		echo form_dropdown('modulo', $moduloindexarray, '1');
+		$moduloindexarray = array();
+		$moduloindexarray['0'] = 'pagos-devel';
+		$moduloindexarray['1'] = 'pagos';
+		$moduloindexarray['2'] = 'bitacora';
+		echo form_dropdown('modulo', $moduloindexarray, '');
 		echo form_submit('login', 'Iniciar sesion');
 		echo form_close() . PHP_EOL;
+		echo br();
+		echo '<center>'.$errl.'</center>';
   
 ?>
